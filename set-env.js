b14a24868393000229bd0a8c159721a995ef7cfe
elianbarrios <elianbarrios1707@outlook.com>
2026-08-21 07:20:09 -0600

    update set-env.js to use JSON.stringyfy()

--- set-env.js
+++ set-env.js
@@ -5,12 +5,11 @@ const targetPath = path.resolve(__dirname, './src/environments/environment.ts');
 
 const envConfigFile = `export const environment = {
   production: ${process.env.PRODUCTION === 'true'},
-  SUPABASE_URL: '${process.env.SUPABASE_URL || ''}',
-  SUPABASE_PUBLISHABLE_KEY: '${process.env.SUPABASE_PUBLISHABLE_KEY || ''}'
+  SUPABASE_URL: ${JSON.stringify(process.env.SUPABASE_URL || '')},
+  SUPABASE_PUBLISHABLE_KEY: ${JSON.stringify(process.env.SUPABASE_PUBLISHABLE_KEY || '')}
 };
 `;
 
-// Asegura que el directorio exista y escribe el archivo
 fs.mkdirSync(path.dirname(targetPath), { recursive: true });
 fs.writeFileSync(targetPath, envConfigFile);
 console.log('environment.ts generado correctamente.');
